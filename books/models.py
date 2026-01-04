648f2c549ba1efbf1e6c5f65981210aa85654dcc
tokh1rovn1 <131213249+tokh1rovn1@users.noreply.github.com>
2026-01-04 14:12:27 +0500

    kitob avtor va category modellari tayyor

--- books/models.py
+++ books/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Kategoriyalar'
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
+    class Meta:
+        verbose_name_plural = 'Mualiflar'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    descriptions = models.TextField(blank=True)
+    cover_image = models.ImageField(upload_to='books/', blank=True, null=True)
+    created_add = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.author}"
+    
+    class Meta:
+        verbose_name_plural = 'kitoblar'
