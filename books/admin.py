e8042406582fbefc9e5ff8a87a8e51cf6750c95d
tokh1rovn1 <131213249+tokh1rovn1@users.noreply.github.com>
2026-01-04 16:09:42 +0500

    admin kuchaytrildii

--- books/admin.py
+++ books/admin.py
@@ -2,6 +2,34 @@ from django.contrib import admin
 from .models import Category, Author, Book
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Author)
-admin.site.register(Book)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+    list_filter = ['name']
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'bio', 'birth_date']
+    search_fields =['name']
+    list_filter = ['birth_date']
+    readonly_fields = ['bio']
+
+
+class CategoryInline(admin.TabularInline):
+    model = Book.category.field.model
+    extra = 1
+
+class AuthorInline(admin.TabularInline):
+    model = Book.author.field.model
+    extra = 1
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = [ 'title', 'author','price', 'category', ]
+    search_fields = ['title', 'author__name',]
+    readonly_fields = ['created_add']
+    #inlines = [AuthorInline, CategoryInline]
+    list_per_page = 10
+    
+
