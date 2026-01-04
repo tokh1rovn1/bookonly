485cfa048422ead8c241d461315b59108f92a6d4
tokh1rovn1 <131213249+tokh1rovn1@users.noreply.github.com>
2026-01-04 23:22:00 +0500

    dizayn yaxshilandi

--- books/views.py
+++ books/views.py
@@ -9,9 +9,20 @@ class BookListView(ListView):
     template_name = 'book_list.html'
     context_object_name = 'books'
 
+    def get_queryset(self):
+        # URL'dan category_id ni olish (masalan: ?category=1)
+        category_id = self.request.GET.get('category')
+        if category_id:
+            # Agar category_id bo'lsa, kitoblarni filtrlaymiz
+            return Book.objects.filter(category_id=category_id)
+        # Agar category_id bo'lmasa, hamma kitoblarni chiqaramiz
+        return Book.objects.all()
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
+        # Tanlangan kategoriyani belgilab qo'yish uchun (optional)
+        context['current_category'] = self.request.GET.get('category')
         return context
     
 
